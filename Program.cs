9e1e1f93c14438e9e35ff4f09c55039c0afea80f
aryareal <aryadwiputra193@gmail.com>
2025-10-06 10:02:20 +0700

    feat: add main program - Added main method for selecting which project to run.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using Sharprompt;
+
+internal class Program
+{
+  static void Main()
+  {
+    // Get all public static methods from classes in assembly
+    var praktikMethods = Assembly.GetExecutingAssembly()
+      .GetTypes()
+      .SelectMany(t => t.GetMethods(BindingFlags.Public | BindingFlags.Static)
+        .Where(m => m.Name.StartsWith("Practice") | m.Name.StartsWith("Assigment") | m.Name.StartsWith("Example") | m.Name.StartsWith("_")))
+      .ToList();
+
+    // Make list of options like "HelloWorld.Praktik1"
+    var options = praktikMethods
+      .Select(m => $"{m.DeclaringType?.Name}.{m.Name}")
+      .ToList();
+
+    // Show prompt
+    var project = Prompt.Select("Choose which project to run", options);
+
+    Console.WriteLine();
+
+    // Separate class name and method
+    var parts = project.Split('.');
+    var className = parts[0];
+    var methodName = parts[1];
+
+    // Find the method
+    var method = praktikMethods
+      .FirstOrDefault(m =>
+        m.DeclaringType?.Name == className &&
+        m.Name == methodName);
+
+    // Run the method
+    method?.Invoke(null, null);
+  }
+}
